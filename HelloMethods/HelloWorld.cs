8d92ccb1e44c82d26d913b86abc6f769fe3d339d
Colleen Blake <colleenemilyblake@gmail.com>
2023-08-24 19:26:59 -0500

    Ch4

---
+++ HelloMethods/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloMethods
+{
+    public class HelloWorld
+    {
+        public string message = "Hello World";
+
+        public void SayHello()
+        {
+
+            string message = "Goodbye World";
+
+            // The line below prints "Goodbye World"
+            Console.WriteLine(message);
+
+            // The line below prints "Hello World"
+            Console.WriteLine(this.message);
+        }
+    }
+}
